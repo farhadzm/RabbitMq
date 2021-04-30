8498f91261c6d64b86d89b4aeadb33e494d3cf1f
farhad zamani <farhad.zamani7720@gmail.com>
2021-04-30 19:37:28 +0430

    Adding consumer layer

---
+++ RabbitMq/RabbitMq.Consumer/Services/ConsumerService.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMq.Common.Services;
+using RabbitMQ.Client.Events;
+using System;
+using RabbitMQ.Client;
+using System.Threading.Tasks;
+
+namespace RabbitMq.Consumer.Services
+{
+    public interface IConsumerService
+    {
+        Task ReadMessgaes();
+    }
+
+    public class ConsumerService : IConsumerService, IDisposable
+    {
+        private readonly IModel _model;
+        private readonly IConnection _connection;
+        public ConsumerService(IRabbitMqService rabbitMqService)
+        {
+            _connection = rabbitMqService.CreateChannel();
+            _model = _connection.CreateModel();
+            _model.QueueDeclare(_queueName, durable: true, exclusive: false, autoDelete: false);
+            _model.ExchangeDeclare("UserExchange", ExchangeType.Fanout, durable: true, autoDelete: false);
+            _model.QueueBind(_queueName, "UserExchange", string.Empty);
+        }
+        const string _queueName = "User";
+        public async Task ReadMessgaes()
+        {
+            var consumer = new AsyncEventingBasicConsumer(_model);
+            consumer.Received += async (ch, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var text = System.Text.Encoding.UTF8.GetString(body);
+                Console.WriteLine(text);
+                await Task.CompletedTask;
+                _model.BasicAck(ea.DeliveryTag, false);
+            };
+            _model.BasicConsume(_queueName, false, consumer);
+            await Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            if (_model.IsOpen)
+                _model.Close();
+            if (_connection.IsOpen)
+                _connection.Close();
+        }
+    }
+}

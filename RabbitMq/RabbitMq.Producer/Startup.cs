15cff8e71ba5f02d30898b201001e2db75345073
farhad zamani <farhad.zamani7720@gmail.com>
2021-04-30 19:37:42 +0430

    adding Producer layer

--- RabbitMq/RabbitMq.Producer/Startup.cs
+++ RabbitMq/RabbitMq.Producer/Startup.cs
@@ -1,21 +1,24 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using RabbitMq.Common.Extensions;
+
 
 namespace RabbitMq.Producer
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
+        public IConfiguration Configuration { get; set; }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCommonService(Configuration);
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {

177953d1a315c3cbd39549962dfaf70f36e42349
farhad zamani <farhad.zamani7720@gmail.com>
2021-04-30 19:37:10 +0430

    [feat] create Common layer

---
+++ RabbitMq/RabbitMq.Common/Extensions/StartupExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RabbitMq.Common.Models;
+using RabbitMq.Common.Services;
+
+namespace RabbitMq.Common.Extensions
+{
+    public static class StartupExtension
+    {
+        public static void AddCommonService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<RabbitMqConfiguration>(a => configuration.GetSection(nameof(RabbitMqConfiguration)).Bind(a));
+            services.AddSingleton<IRabbitMqService, RabbitMqService>();
+        }
+    }
+}
---
+++ RabbitMq/RabbitMq.Common/Services/RabbitMqService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Options;
+using RabbitMq.Common.Models;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMq.Common.Services
+{
+    public interface IRabbitMqService
+    {
+        IConnection CreateChannel();
+    }
+
+    public class RabbitMqService : IRabbitMqService
+    {
+        private readonly RabbitMqConfiguration _configuration;
+        public RabbitMqService(IOptions<RabbitMqConfiguration> options)
+        {
+            _configuration = options.Value;
+        }
+        public IConnection CreateChannel()
+        {
+            ConnectionFactory connection = new ConnectionFactory()
+            {
+                UserName = _configuration.Username,
+                Password = _configuration.Password,
+                HostName = _configuration.HostName
+            };
+            connection.DispatchConsumersAsync = true;
+            var channel = connection.CreateConnection();
+            return channel;
+        }
+    }
+}
